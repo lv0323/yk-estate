d7a23c9029ee8db413cf50124defbf1e3db662e3
zhangrongfan <ljs123456>
2016-12-20 16:59:39 +0800

    add oss

--- estate/estate-rest/src/main/java/com/lyun/estate/rest/test/TestController.java
+++ estate/estate-rest/src/main/java/com/lyun/estate/rest/test/TestController.java
@@ -1,13 +1,21 @@
 package com.lyun.estate.rest.test;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import com.lyun.estate.biz.Oss;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.io.File;
 
 @RestController
 @RequestMapping("/test")
 public class TestController {
+
+    private final Oss oss;
+
+    @Autowired
+    public TestController(Oss oss) {
+        this.oss = oss;
+    }
 
     @GetMapping(value = "/string")
     @ResponseBody
@@ -15,4 +23,9 @@ public class TestController {
         return "string";
     }
 
+    @RequestMapping("oss/{file}")
+    public String oss(@PathVariable String file) {
+        file += ".jpg";
+        return oss.upload(file, new File("D:\\" + file));
+    }
 }
