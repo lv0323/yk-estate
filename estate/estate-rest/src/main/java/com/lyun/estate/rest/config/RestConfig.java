3e524c6764d0c9aa174b6302f72dd56774c18a12
taoshiyu <690934169@qq.com>
2016-12-22 18:28:15 +0800

    feature(swagger2): enable

--- estate/estate-rest/src/main/java/com/lyun/estate/rest/config/RestConfig.java
+++ estate/estate-rest/src/main/java/com/lyun/estate/rest/config/RestConfig.java
@@ -6,11 +6,13 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.PropertySource;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
 @ComponentScan({"com.lyun.estate.rest"})
 @Import({CoreConfig.class, BizConfig.class})
 @PropertySource(value = "classpath:application.properties", ignoreResourceNotFound = true)
+@EnableSwagger2
 public class RestConfig {
 
 }
