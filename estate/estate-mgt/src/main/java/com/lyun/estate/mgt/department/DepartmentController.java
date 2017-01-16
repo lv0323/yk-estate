d4e01655bdc82b7d8a3224a7034bc9edc111255e
zhangrongfan <ljs123456>
2017-01-16 11:55:15 +0800

    SORT
    (cherry picked from commit 965502f)

--- estate/estate-mgt/src/main/java/com/lyun/estate/mgt/department/DepartmentController.java
+++ estate/estate-mgt/src/main/java/com/lyun/estate/mgt/department/DepartmentController.java
@@ -6,7 +6,7 @@ import com.lyun.estate.mgt.employee.LoginEmployee;
 import com.lyun.estate.mgt.supports.RestResponse;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Objects;
+import java.util.*;
 
 @RestController
 @RequestMapping("api/department")
@@ -42,5 +42,40 @@ public class DepartmentController {
     @GetMapping("query")
     public Object query(@SessionAttribute LoginEmployee employee) {
         return service.selectByCompanyId(employee.getCompanyId());
+    }
+
+    @GetMapping("query-sorted")
+    public Object querySorted(@SessionAttribute LoginEmployee employee) {
+        List<Department> departmentList = service.selectByCompanyId(employee.getCompanyId());
+        List<Map<String, Object>> sortedList = new ArrayList<>(departmentList.size());
+        Map<Long, Map<String, Object>> departmentMap = new HashMap<>();
+        departmentMap.put(null, null);
+        Map<Long, Map<String, Object>> nextDepartmentMap = new HashMap<>();
+        int level = 0;
+        while (!departmentList.isEmpty()) {
+            Iterator<Department> iterator = departmentList.iterator();
+            while (iterator.hasNext()) {
+                Department depart = iterator.next();
+                if (!departmentMap.containsKey(depart.getParentId()))
+                    continue;
+
+                Map<String, Object> item = new HashMap<>();
+                item.put("level", level);
+                item.put("department", depart);
+
+                Map<String, Object> parent = departmentMap.get(depart.getParentId());
+                if (parent == null)
+                    sortedList.add(item);
+                else
+                    sortedList.add(sortedList.indexOf(parent) + 1, item);
+
+                iterator.remove();
+                nextDepartmentMap.put(depart.getId(), item);
+            }
+            departmentMap = nextDepartmentMap;
+            nextDepartmentMap = new HashMap<>();
+            level++;
+        }
+        return sortedList;
     }
 }
