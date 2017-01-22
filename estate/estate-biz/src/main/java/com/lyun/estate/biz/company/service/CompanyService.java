5af9ff662873db4968451135740859e1bf97c688
zhangrongfan <ljs123456>
2017-01-22 17:11:00 +0800

    delete facade

--- estate/estate-biz/src/main/java/com/lyun/estate/biz/company/service/CompanyService.java
+++ estate/estate-biz/src/main/java/com/lyun/estate/biz/company/service/CompanyService.java
@@ -1,10 +1,16 @@
 package com.lyun.estate.biz.company.service;
 
 import com.lyun.estate.biz.company.entity.Company;
+import com.lyun.estate.biz.company.entity.CreateCompanyEntity;
 import com.lyun.estate.biz.company.repo.CompanyRepository;
+import com.lyun.estate.biz.department.entity.Department;
+import com.lyun.estate.biz.department.service.DepartmentService;
+import com.lyun.estate.biz.employee.def.Status;
+import com.lyun.estate.biz.employee.service.EmployeeService;
 import com.lyun.estate.core.utils.Validations;
 import org.apache.ibatis.session.RowBounds;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Date;
 import java.util.List;
@@ -14,9 +20,13 @@ import java.util.Objects;
 public class CompanyService {
 
     private final CompanyRepository repository;
+    private final EmployeeService employeeService;
+    private final DepartmentService departmentService;
 
-    public CompanyService(CompanyRepository repository) {
+    public CompanyService(CompanyRepository repository, EmployeeService employeeService, DepartmentService departmentService) {
         this.repository = repository;
+        this.employeeService = employeeService;
+        this.departmentService = departmentService;
     }
 
     public Company create(Company company) {
@@ -42,5 +52,23 @@ public class CompanyService {
 
     public List<Company> find(RowBounds rowBounds) {
         return repository.select(rowBounds);
+    }
+
+    @Transactional
+    public Company createCompany(CreateCompanyEntity entity) {
+        create(entity.getCompany());
+
+        Department department = new Department()
+                .setCompanyId(entity.getCompany().getId())
+                .setName(entity.getCompany().getName())
+                .setShortName(entity.getCompany().getShortName())
+                .setAddress(entity.getCompany().getAddress());
+        departmentService.create(department);
+
+        employeeService.createBoss(entity.getBoss()
+                .setCompanyId(entity.getCompany().getId())
+                .setDepartmentId(department.getId())
+                .setStatus(Status.WORKING));
+        return entity.getCompany();
     }
 }
--- estate/estate-mgt/src/main/java/com/lyun/estate/mgt/company/CompanyController.java
+++ estate/estate-mgt/src/main/java/com/lyun/estate/mgt/company/CompanyController.java
@@ -3,7 +3,6 @@ package com.lyun.estate.mgt.company;
 import com.lyun.estate.biz.company.entity.Company;
 import com.lyun.estate.biz.company.entity.CreateCompanyEntity;
 import com.lyun.estate.biz.company.service.CompanyService;
-import com.lyun.estate.biz.company.service.CompanyServiceFacade;
 import com.lyun.estate.mgt.supports.RestResponse;
 import org.apache.ibatis.session.RowBounds;
 import org.springframework.web.bind.annotation.*;
@@ -14,36 +13,34 @@ import java.sql.Date;
 @RequestMapping("api/company")
 public class CompanyController {
 
-    private final CompanyService companyService;
-    private final CompanyServiceFacade companyServiceFacade;
+    private final CompanyService service;
 
-    public CompanyController(CompanyService companyService, CompanyServiceFacade companyServiceFacade) {
-        this.companyService = companyService;
-        this.companyServiceFacade = companyServiceFacade;
+    public CompanyController(CompanyService service) {
+        this.service = service;
     }
 
     @PostMapping("create")
     public Object create(CreateCompanyEntity entity) {
-        return companyServiceFacade.createCompany(entity);
+        return service.createCompany(entity);
     }
 
     @GetMapping("lock")
     public Object lock(@RequestParam Long id, @RequestParam Boolean locked) {
-        return new RestResponse().add("ret", companyService.lock(id, locked)).get();
+        return new RestResponse().add("ret", service.lock(id, locked)).get();
     }
 
     @GetMapping("renew")
     public Object renew(@RequestParam Long id, @RequestParam Date endDate) {
-        return new RestResponse().add("ret", companyService.renew(id, endDate)).get();
+        return new RestResponse().add("ret", service.renew(id, endDate)).get();
     }
 
     @PostMapping("edit")
     public Object edit(Company company) {
-        return companyService.update(company);
+        return service.update(company);
     }
 
     @GetMapping("query")
     public Object queryAll(@RequestParam(required = false, defaultValue = "0") int offset, @RequestParam(required = false, defaultValue = "2147483647") int limit) {
-        return companyService.find(new RowBounds(offset, limit));
+        return service.find(new RowBounds(offset, limit));
     }
 }
