e2ae0ff891c9621e9cf488c10d94a801e9b6ce87
zhangrongfan <ljs123456>
2017-01-22 11:56:10 +0800

    CookieSession

--- estate/estate-mgt/src/main/java/com/lyun/estate/mgt/employee/EmployeeController.java
+++ estate/estate-mgt/src/main/java/com/lyun/estate/mgt/employee/EmployeeController.java
@@ -5,10 +5,11 @@ import com.lyun.estate.biz.auth.sms.SmsCode;
 import com.lyun.estate.biz.auth.sms.SmsCodeArgumentResolver;
 import com.lyun.estate.biz.employee.entity.Employee;
 import com.lyun.estate.biz.employee.service.EmployeeService;
-import com.lyun.estate.biz.message.service.SmsService;
 import com.lyun.estate.core.supports.exceptions.EstateException;
 import com.lyun.estate.core.supports.exceptions.ExCode;
 import com.lyun.estate.mgt.supports.RestResponse;
+import org.springframework.session.Session;
+import org.springframework.session.SessionRepository;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -21,11 +22,12 @@ import java.util.Objects;
 public class EmployeeController {
 
     private final EmployeeService employeeService;
-    private final SmsService smsService;
+    private final SessionRepository<Session> sessionRepository;
 
-    public EmployeeController(EmployeeService employeeService, SmsService smsService) {
+    @SuppressWarnings("unchecked")
+    public EmployeeController(EmployeeService employeeService, SessionRepository sessionRepository) {
         this.employeeService = employeeService;
-        this.smsService = smsService;
+        this.sessionRepository = sessionRepository;
     }
 
     @PostMapping("create")
@@ -71,23 +73,28 @@ public class EmployeeController {
     public Object salt(@RequestParam String mobile) {
         Employee employee = employeeService.selectByMobile(mobile);
         if (employee == null)
-            return new EstateException(ExCode.CANT_SALT);
+            throw new EstateException(ExCode.CANT_SALT);
         return new RestResponse().add("ret", true).add("salt", employee.getSalt()).add("sugar", employeeService.sugar(mobile)).get();
     }
 
     @GetMapping("login")
-    public Object login(@RequestParam String mobile, @RequestParam String password, HttpSession session) {
+    public Object login(@RequestParam String mobile, @RequestParam String password, HttpSession cookieSession) {
         Employee employee = employeeService.login(mobile, password);
-        if (employee == null)
-            return new RestResponse().add("ret", false).get();
+        cookieSession.setAttribute("employee", true);
+        Session session = sessionRepository.createSession();
+        session.setAttribute("cookieSession", cookieSession.getId());
         session.setAttribute("employee", new LoginEmployee(employee.getId(), employee.getCompanyId(),
                 employee.getDepartmentId(), employee.getPositionId(), employee.getIsBoss(), employee.getIsAgent(),
                 employee.getMobile(), employee.getName(), employee.getGender()));
+        sessionRepository.save(session);
         return new RestResponse().add("ret", true).add("token", session.getId()).get();
     }
 
     @GetMapping("logout")
     public Object logout(HttpSession session, @SessionAttribute LoginEmployee employee) {
+        Session cookieSession = sessionRepository.getSession((String) session.getAttribute("cookieSession"));
+        cookieSession.removeAttribute("employee");
+        sessionRepository.save(cookieSession);
         session.invalidate();
         return new RestResponse().add("ret", true).get();
     }
