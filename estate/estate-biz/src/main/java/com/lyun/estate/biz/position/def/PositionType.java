c5b317f21f08c727f302d99db288910838572b19
Jeffrey Chiang <jiang3.jl@gmail.com>
2017-02-14 10:48:28 +0800

    add admin

--- estate/estate-biz/src/main/java/com/lyun/estate/biz/position/def/PositionType.java
+++ estate/estate-biz/src/main/java/com/lyun/estate/biz/position/def/PositionType.java
@@ -5,6 +5,7 @@ package com.lyun.estate.biz.position.def;
  */
 public enum PositionType {
     BUSINESS("业务"),
+    ADMIN("管理员"),
     DEPT_M("店管"),
     REGION_M("区管"),
     OPERATION("运营"),
@@ -15,5 +16,9 @@ public enum PositionType {
 
     PositionType(String label) {
         this.label = label;
+    }
+
+    public String getLabel() {
+        return label;
     }
 }
