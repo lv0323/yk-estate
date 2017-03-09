6cbc2e2d513c851b266cde792090c613dfe287fe
nora <hongyang@lyun.com>
2017-03-09 11:09:13 +0800

    closePropVisit

--- estate/estate-mgt/src/main/webapp/resources/js/app/propertyVisit/propertyVisit.js
+++ estate/estate-mgt/src/main/webapp/resources/js/app/propertyVisit/propertyVisit.js
@@ -4,8 +4,10 @@
 require(['main-app',
         contextPath + '/js/service/propertyvisit-service.js',
         contextPath + '/js/plugins/pagination/pagingPlugin.js',
+        contextPath+'/js/service/util-service.js',
+        contextPath + '/js/utils/dataTableHelp.js',
         'datepicker.zh-cn', 'sweetalert', 'datatables', 'datatablesBootstrap'],
-    function (mainApp, PropertyVisitService, pagingPlugin) {
+    function (mainApp, PropertyVisitService, pagingPlugin, UtilService, dataTableHelp) {
 
         var pageConfig = {
             limit: 8,
@@ -30,12 +32,16 @@ require(['main-app',
         var displayTable = function (data) {
             var dataSet = data.items.map(function (item, index) {
                 return {
-                   /* employeeName: item.,
-                    customerName: item.,
-                    propertyId: item.,
-                    visitStartDate: item.,
-                    visitEndDate: item.,
-                    status: item.*/
+                    employee: "<img class='img-circle' style='width:42px; height:42px; display: inline-block;' src='"+item.avatarURI+"'/>"+"&nbsp;"+item.departmentName+"-"+item.employeeName,
+                    customerName: item.customerTiny.name,
+                    propertyAddress: item.fangTiny.houseLicence.location,
+                    visitStartDate: UtilService.timeStamp2Datetime(item.createTime),
+                    visitUpdDate: UtilService.timeStamp2Datetime(item.updateTime),
+                    status: (item.process.name==="CREATED")?"<label class='badge badge-info'>"+item.process.label+"</label>":(item.process.name==="SUCCESS")?"<label class='badge badge-success'>"+item.process.label+"</label>":"<label class='badge badge-warning'>"+item.process.label+"</label>",
+                    operation:(item.process.name==="CREATED")?
+                        [{attr: {class: 'btn completeVisitBtn'}, data: {index: index, id: item.id, toggle: 'modal', target:'#completeVisitDialog'}, text: '完成'},
+                            {attr: {class: 'btn cancelVisitBtn'}, data: {index: index, id: item.id, toggle: 'modal', target:'#cancelVisitDialog'}, text: '取消'}
+                        ]:""
                 }
             });
 
@@ -48,15 +54,16 @@ require(['main-app',
                     ordering: false,
                     autoWidth: false,
                     columnDefs: [
-                        {className: "text-right", "targets": [5]} /*添加class*/
+                        {className: "text-right", "targets": [6]} /*添加class*/
                     ],
                     columns: [
-                        {title: "带看员工", data: 'employeeName'},
+                        {title: "带看员工", data: 'employee'},
                         {title: "带看客户", data: 'customerName', defaultContent: ""},
-                        {title: "带看房源编号", data: 'propertyId', defaultContent: ""},
+                        {title: "带看房源地址", data: 'propertyAddress', defaultContent: ""},
                         {title: "带看生成时间", data: 'visitStartDate', defaultContent: ""},
-                        {title: "带看结束时间", data: 'visitEndDate', defaultContent: ""},
-                        {title: "状态", data: 'status', defaultContent: ""}
+                        {title: "带看更新时间", data: 'visitUpdDate', defaultContent: ""},
+                        {title: "状态", data: 'status', defaultContent: ""},
+                        {title: "操作", data:'operation', "render": dataTableHelp.operationFormat()}
                     ]
                 });
             } else {
@@ -84,8 +91,8 @@ require(['main-app',
             pagingPlugin.init(config);
         };
 
-        function getPropertyVisit(offset, limit) {
-            PropertyVisitService.getPropertyVisitList({'x-paging': 'total=true&offset='+offset+'&limit=' + limit})
+        function getPropertyVisit(params, offset, limit) {
+            PropertyVisitService.getPropertyVisitList(params, {'x-paging': 'total=true&offset='+offset+'&limit=' + limit})
                 .done(function (data) {
                     displayTable(data);
                     pagination(data.total);
@@ -95,7 +102,7 @@ require(['main-app',
                 });
         }
 
-        getPropertyVisit(0, pageConfig.limit);
+        getPropertyVisit(null, 0, pageConfig.limit);
 
         //toggle filter for Employee display
         $('#filterPropertyVisitBtn').on('click',function () {
@@ -104,6 +111,52 @@ require(['main-app',
             }else {
                 $('#box-filter').hide();
             }
+        });
+
+        //initialize complete visit dialog
+        $('.fadeInRight').on('click','.completeVisitBtn',function(e){
+            var visitId = $(e.target).data('id');
+            $('#completeVisitId').val(visitId);
+        });
+
+        //action for complete visit
+        $('#confirmCompleteVisitBtn').on('click', function(){
+            var visitId = $('#completeVisitId').val();
+            var toUpdateData = {
+                showingId: visitId,
+                process: 'SUCCESS'
+            };
+
+            PropertyVisitService.closePropertyVisit(toUpdateData)
+                .done(function () {
+                    location.reload(true);
+                })
+                .fail(function () {
+
+                });
+        });
+
+        //initialize cancel visit dialog
+        $('.fadeInRight').on('click','.cancelVisitBtn',function(e){
+            var visitId = $(e.target).data('id');
+            $('#cancelVisitId').val(visitId);
+        });
+
+        //action for cancel visit
+        $('#confirmCancelVisitBtn').on('click', function(){
+            var visitId = $('#cancelVisitId').val();
+            var toUpdateData = {
+                showingId: visitId,
+                process: 'CANCEL'
+            };
+
+            PropertyVisitService.closePropertyVisit(toUpdateData)
+                .done(function () {
+                    location.reload(true);
+                })
+                .fail(function () {
+
+                });
         });
 
 
