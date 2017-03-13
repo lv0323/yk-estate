9ef9721f78ce32615a068fa4507091b05812e365
liruibin <liruibin@lvjinsuo.com>
2017-03-13 16:38:04 +0800

    set body Padding-right during alert

--- estate/estate-mgt/src/main/webapp/resources/js/plugins/SweetAlert/SweetAlertHelp.js
+++ estate/estate-mgt/src/main/webapp/resources/js/plugins/SweetAlert/SweetAlertHelp.js
@@ -9,22 +9,38 @@ define(contextPath+'/js/plugins/SweetAlert/SweetAlertHelp.js',
 
 
         SweetAlertHelp.success = function(res, callback){
+            var widthDiffs = window.innerWidth - $("body").eq(0).prop('clientWidth');
             swal({
                 title: (res && res.title) || "操作成功!",
                 type: "success",
                 text: (res && res["message"]) || '',
                 confirmButtonText: (res && res.confirmButtonText) || "确定",
                 confirmButtonColor: (res && res.confirmButtonColor) ||  "#3c8dbc"
-            },callback);
+            },function(){
+                $("body").css({"padding-right":''});
+                callback && callback()
+            });
+            var scrollBarWidth = widthDiffs - (window.innerWidth - $("body").eq(0).prop('clientWidth'));
+            if (scrollBarWidth > 0) {
+                $("body").css({"padding-right":scrollBarWidth+'px'});
+            }
         }
         SweetAlertHelp.fail = function(res, callback) {
+            var widthDiffs = window.innerWidth - $("body").eq(0).prop('clientWidth');
             swal({
                 title: (res && res.title)|| "错误!",
                 text: (res && res["message"]) || '',
                 type: "error",
                 confirmButtonText: res.confirmButtonText || "确定",
                 confirmButtonColor: res.confirmButtonColor || "#3c8dbc"
-            },callback);
+            },function(){
+                $("body").css({"padding-right":''});
+                callback && callback()
+            });
+            var scrollBarWidth = widthDiffs - (window.innerWidth - $("body").eq(0).prop('clientWidth'));
+            if (scrollBarWidth > 0) {
+                $("body").css({"padding-right":scrollBarWidth+'px'});
+            }
         };
 
         return SweetAlertHelp;
