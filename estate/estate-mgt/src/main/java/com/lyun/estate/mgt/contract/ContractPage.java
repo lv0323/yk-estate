d40e0c66f32d993850f09ec466614ef57b6b8d12
nora <hongyang@lyun.com>
2017-03-15 11:08:43 +0800

    addDeal

--- estate/estate-mgt/src/main/java/com/lyun/estate/mgt/contract/ContractPage.java
+++ estate/estate-mgt/src/main/java/com/lyun/estate/mgt/contract/ContractPage.java
@@ -1,18 +1,17 @@
 package com.lyun.estate.mgt.contract;
-
 import com.lyun.estate.biz.contract.def.ContractDefine.Process;
 import com.lyun.estate.mgt.employee.service.EmployeeMgtService;
 import com.lyun.estate.biz.fang.def.HouseType;
 import com.lyun.estate.biz.support.def.BizType;
+import com.lyun.estate.biz.fang.def.PriceUnit;
+import com.lyun.estate.biz.support.def.IdentitySource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
-
 import java.util.Arrays;
 import java.util.HashMap;
-
 /**
  * Created by yanghong on 3/13/17.
  */
@@ -21,7 +20,6 @@ import java.util.HashMap;
 public class ContractPage {
     @Autowired
     private EmployeeMgtService service;
-
     @GetMapping("/deal")
     public ModelAndView deal() {
         HashMap<String, Object> params = new HashMap<>();
@@ -32,7 +30,6 @@ public class ContractPage {
         params.put("bizType",Arrays.asList(BizType.values()));
         return new ModelAndView("/contract/deal/deal", params);
     }
-
     @GetMapping("/addDeal")
     public ModelAndView addDeal() {
         HashMap<String, Object> params = new HashMap<>();
@@ -40,6 +37,8 @@ public class ContractPage {
         params.put("username",service.getUsername());
         params.put("houseType",Arrays.asList(HouseType.values()));
         params.put("bizType",Arrays.asList(BizType.values()));
+        params.put("priceUnit",Arrays.asList(PriceUnit.values()));
+        params.put("identitySource",Arrays.asList(IdentitySource.values()));
         return new ModelAndView("/contract/deal/addDeal", params);
     }
 }
---
+++ estate/estate-mgt/src/main/webapp/resources/js/app/contract/deal/addDeal.js
@@ -0,0 +1,119 @@
+/**
+ * Created by yanghong on 3/15/17.
+ */
+/**
+ * Created by yanghong on 3/14/17.
+ */
+require(['main-app',
+        contextPath + '/js/service/contract-service.js',
+        contextPath + '/js/service/propertyvisit-service.js',
+        contextPath + '/js/service/fang-service.js',
+        contextPath + '/js/plugins/SweetAlert/SweetAlertHelp.js'],
+    function (mainApp, ContractService, PropertyVisitService, FangService, SweetAlertHelp) {
+        //var houseLicenceID = window.location.href.split('?')[1];
+        function verifyAddDeal(toSubmitDeal) {
+            var flag = true;
+            $('.form-group').find('.invalid-input').removeClass('invalid-input');
+            if (toSubmitDeal.fangId === "" || typeof(toSubmitDeal.fangId) === 'undefined') {
+                flag = false;
+                $('#houseLicenceID').addClass('invalid-input');
+                /*$('#certifAddress').addClass('invalid-input');
+                 $('#certifNo').addClass('invalid-input');*/
+                $('#areaSize').addClass('invalid-input');
+                $('#totalPrice').addClass('invalid-input');
+            }
+            if (toSubmitDeal.assignorName === "" || typeof(toSubmitDeal.assignorName) === 'undefined') {
+                flag = false;
+                $('#OwnerName').addClass('invalid-input');
+            }
+            if (toSubmitDeal.assignorIdNo === "" || typeof(toSubmitDeal.assignorIdNo) === 'undefined') {
+                flag = false;
+                $('#OwnerIdNo').addClass('invalid-input');
+            }
+            if (toSubmitDeal.assignorMobile === "" || typeof(toSubmitDeal.assignorMobile) === 'undefined') {
+                flag = false;
+                $('#OwnerContact').addClass('invalid-input');
+            }
+            if (toSubmitDeal.assigneeName === "" || typeof(toSubmitDeal.assigneeName) === 'undefined') {
+                flag = false;
+                $('#customerName').addClass('invalid-input');
+            }
+            if (toSubmitDeal.assigneeIdNo === "" || typeof(toSubmitDeal.assigneeIdNo) === 'undefined') {
+                flag = false;
+                $('#customerIdNo').addClass('invalid-input');
+            }
+            if (toSubmitDeal.assigneeMobile === "" || typeof(toSubmitDeal.assigneeMobile) === 'undefined') {
+                flag = false;
+                $('#customerContact').addClass('invalid-input');
+            }
+            return flag;
+        }
+        $('#getHouseInfoBtn').on('click',function () {
+            $('.form-group').find('.invalid-input').removeClass('invalid-input');
+            var houseLicenceID = $('#houseLicenceID').val();
+            PropertyVisitService.getPropertyInfo({licenceId:houseLicenceID})
+                .done(function (data) {
+                    $('#fangID').val(data.id);
+                    $('#areaSize').val(data.estateArea);
+                    $('#totalPrice').val(data.publishPrice);
+                    $('#priceUnit').val(data.priceUnit.name);
+                    $('#houseType').val(data.houseType.name);
+                    $('#businessType').val(data.bizType.name);
+                    var fangID = $('#fangID').val();
+                    FangService.ext({fangId:fangID})
+                        .done(function (data) {
+                            $('#certifAddress').val(data.certifAddress);
+                            $('#certifNo').val(data.certifNo);
+                        })
+                        .fail(function (res) {
+                            SweetAlertHelp.fail(res);
+                        });
+                    FangService.contact({fangId:fangID})
+                        .done(function (data) {
+                            $('#OwnerName').val(data.name);
+                            $('#OwnerContact').val(data.mobile);
+                        })
+                        .fail(function (res) {
+                            SweetAlertHelp.fail(res);
+                        });
+                })
+                .fail(function (res) {
+                    SweetAlertHelp.fail(res);
+                    $('input').val("");
+                });
+        });
+        $('#confirmAddDealBtn').on('click',function () {
+            var toAddDeal = {
+                fangId: $('#fangID').val(),
+                contractType: 'DEAL',
+                houseType: $('#houseType option:selected').val(),
+                certifAddress: $('#certifAddress').val(),
+                certifNo: $('#certifNo').val(),
+                estateArea: $('#areaSize').val(),
+                price: $('#totalPrice').val(),
+                priceUnit: $('#priceUnit option:selected').val(),
+                bizType: $('#businessType option:selected').val(),
+                assignorName: $('#OwnerName').val(),
+                assignorMobile: $('#OwnerContact').val(),
+                assignorIdSource: $('#OwnerIdSource option:selected').val(),
+                assignorIdNo: $('#OwnerIdNo').val(),
+                assigneeName: $('#customerName').val(),
+                assigneeMobile: $('#customerContact').val(),
+                assigneeIdSource: $('#customerIdSource option:selected').val(),
+                assigneeIdNo: $('#customerIdNo').val(),
+                note: $('#note').val()
+            };
+            if(verifyAddDeal(toAddDeal)){
+                ContractService.addDeal(toAddDeal)
+                    .done(function () {
+                        SweetAlertHelp.success({}, function () {
+                            window.location.href="/mgt/contract/deal";
+                        });
+                    }).fail(function (res) {
+                    SweetAlertHelp.fail(res);
+                });
+            }else {
+                SweetAlertHelp.fail({message:"请填写所有必填字段"});
+            }
+        });
+    });
