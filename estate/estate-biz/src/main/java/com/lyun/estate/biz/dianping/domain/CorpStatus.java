945a323c4dcd37f356c1fb0eeb7c92ae68766337
zhuhuaqiang <zhuhuaqiang@lvjinsuo.com>
2017-06-15 16:12:51 +0800

    refactor package

--- estate/estate-biz/src/main/java/com/lyun/estate/biz/dianping/domain/CorpStatus.java
+++ estate/estate-biz/src/main/java/com/lyun/estate/biz/dianping/domain/CorpStatus.java
@@ -4,5 +4,6 @@ package com.lyun.estate.biz.dianping.domain;
  * Created by localuser on 2017/6/13.
  */
 public enum CorpStatus {
-    NEW, ACTIVIE, SUSPEND/*, REJECTED*/
+
+    NEW, ACTIVE, SUSPEND/*, REJECTED*/
 }
