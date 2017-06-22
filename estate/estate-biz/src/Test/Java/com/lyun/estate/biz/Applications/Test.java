76ad4ab9fe8363194b8f1fef8521b9549a908224
ZhouXiaoming <Xiaoming@Xiaomings-MacBook.local>
2017-06-22 17:34:53 +0800

    added test case

--- estate/estate-biz/src/Test/Java/com/lyun/estate/biz/Applications/Test.java
+++ estate/estate-biz/src/Test/Java/com/lyun/estate/biz/Applications/Test.java
@@ -1,6 +1,15 @@
 package com.lyun.estate.biz.Applications;
 
+import com.lyun.estate.biz.config.BizConfig;
+import com.lyun.estate.core.config.CoreConfig;
 import org.junit.runner.RunWith;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = {BizConfig.class, CoreConfig.class})
 
 public class Test {
 }
