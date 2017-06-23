f1efe7e1e9cc39fd1bade00cb7b09e7e11985195
ZhouXiaoming <Xiaoming@Xiaomings-MacBook.local>
2017-06-23 16:46:38 +0800

    changed code format

--- estate/estate-mgt/src/main/java/com/lyun/estate/mgt/fang/FangProcessRest.groovy
+++ estate/estate-mgt/src/main/java/com/lyun/estate/mgt/fang/FangProcessRest.groovy
@@ -27,49 +27,49 @@ class FangProcessRest {
 
     @PostMapping('publish')
     CommonApplicationEntity requestPublish(@RequestParam Long fangId,
-                 @RequestParam String applyReason) {
+                                           @RequestParam String applyReason) {
         return processMgtService.requestPublish(fangId, applyReason)
     }
 
     @PostMapping('un-publish')
     CommonApplicationEntity requestUnPublish(@RequestParam Long fangId,
-                          @RequestParam String applyReason) {
+                                             @RequestParam String applyReason) {
         return processMgtService.requestUnPublish(fangId, applyReason)
     }
 
     @PostMapping('pause')
     CommonApplicationEntity requestPause(@RequestParam Long fangId,
-               @RequestParam String applyReason) {
+                                         @RequestParam String applyReason) {
         return processMgtService.requestPause(fangId, applyReason)
     }
 
     @PostMapping('apply-public')
     CommonApplicationEntity requestApplyPublic(@RequestParam Long fangId,
-                     @RequestParam String applyReason) {
+                                               @RequestParam String applyReason) {
         return processMgtService.requestPublic(fangId, applyReason)
     }
 
     @PostMapping('undo-public')
     CommonApplicationEntity requestUndoPublic(@RequestParam Long fangId,
-                           @RequestParam String applyReason) {
+                                              @RequestParam String applyReason) {
         return processMgtService.requestUndoPublic(fangId, applyReason)
     }
 
     @PostMapping('approve')
     int approveApplication(@RequestParam Long applicationId,
-                                               @RequestParam String reviewerComments) {
+                           @RequestParam String reviewerComments) {
         processMgtService.approve(applicationId, reviewerComments)
     }
 
     @PostMapping('reject')
     int rejectApplication(@RequestParam Long applicationId,
-                            @RequestParam String reviewerComments) {
+                          @RequestParam String reviewerComments) {
         processMgtService.reject(applicationId, reviewerComments)
     }
 
     @PostMapping('close')
     int closeApplication(@RequestParam Long applicationId,
-                            @RequestParam String reviewerComments) {
+                         @RequestParam String reviewerComments) {
         processMgtService.close(applicationId, reviewerComments)
     }
 
@@ -84,13 +84,13 @@ class FangProcessRest {
     }
 
     @GetMapping("/all")
-    List<CommonApplicationEntity> getApplications(@RequestParam CommonApplicationEntity.Type type,
-                                                         @RequestParam long id,
-                                                         @RequestParam long applicantId,
-                                                         @RequestParam CommonApplicationEntity.Status status,
-                                                         @RequestParam(required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) Date startTime,
-                                                         @RequestParam(required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) Date endTime,
-                                                         @RequestHeader(PageBoundsArgumentResolver.PAGE_HEADER) PageBounds pageBounds) {
+    List<CommonApplicationEntity> getApplications(@RequestParam(required = false) CommonApplicationEntity.Type type,
+                                                  @RequestParam(required = false) long id,
+                                                  @RequestParam(required = false) long applicantId,
+                                                  @RequestParam(required = false) CommonApplicationEntity.Status status,
+                                                  @RequestParam(required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) Date startTime,
+                                                  @RequestParam(required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) Date endTime,
+                                                  @RequestHeader(PageBoundsArgumentResolver.PAGE_HEADER) PageBounds pageBounds) {
         return processMgtService.findApplications(type, id, applicantId, status, startTime, endTime, pageBounds);
     }
 }
